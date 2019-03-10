2df8001469c83cab82bfa5af88a1c017ecc30887
Zach Garwood <zachgarwood@gmail.com>
2019-03-09 22:10:59 -0600

    Refined post-install hook

--- blueprints/ember-cli-auto-generate-addon-npm/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  description: 'Sets up an NPM postinstall hook for generating Ember addons',
-
-  normalizeEntityName() {}, // no-op
-};
---
+++ blueprints/ember-cli-auto-generate-addon/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const fs = require('fs');
+
+const POST_INSTALL_HOOK = 'node_modules/.hooks/postinstall';
+
+module.exports = {
+  description: 'Sets up an NPM postinstall hook for generating Ember addons',
+  normalizeEntityName() {/* no-op */},
+  afterInstall() {
+    return fs.chmodSync(POST_INSTALL_HOOK, 0o755);
+  },
+  beforeUninstall() {
+    return fs.unlinkSync(POST_INSTALL_HOOK);
+  }
+};
